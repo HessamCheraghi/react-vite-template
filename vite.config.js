20933aaa875ecc83e659d6870fcff3a6686b0f0b
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-10-29 02:16:18 +0330

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve("./src"),
+    },
+  },
+  base: "./",
+});
